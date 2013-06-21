e3929d624188a9e56d94d55b48a983afbca300eb
Patrick Toohey <ptoohey@oswego.edu>
2013-06-21 14:41:04 -0400

    postsharp getting started tutorial

---
+++ PostSharp/1. GettingStarted/GettingStarted/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PostSharp.Tutorial
+{
+    using System;
+    using System.Diagnostics;
+
+    internal static class Program
+    {
+            private static void Main()
+        {
+                Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+                SayHello();
+                SayGoodbye();
+            }
+
+            [Trace("MyCategory")]
+            private static void SayHello()
+            {
+                Console.WriteLine("Hello, world.");
+            }
+
+            [Trace("MyCategory")]
+            private static void SayGoodbye()
+            {
+                Console.WriteLine("Good bye, world.");
+        }
+    }
+}
---
+++ PostSharp/1. GettingStarted/GettingStarted/TraceAttribute.cs
@@ -0,0 +1,32 @@
+﻿namespace PostSharp.Tutorial
+{
+    using System;
+    using System.Diagnostics;
+
+    using PostSharp.Aspects;
+
+    [Serializable]
+    public class TraceAttribute : OnMethodBoundaryAspect
+    {
+        private readonly string category;
+
+        public TraceAttribute(string category)
+        {
+            this.category = category;
+        }
+
+        public string Category { get { return this.category; } }
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Trace.WriteLine(string.Format("Entering {0}.{1}",
+                args.Method.DeclaringType.Name, args.Method.Name), this.category);
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Trace.WriteLine(string.Format("Leaving {0}.{1}",
+                args.Method.DeclaringType.Name, args.Method.Name), this.category);
+        }
+    }
+}
