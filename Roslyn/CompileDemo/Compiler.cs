52117f7ea9930d825f4c6e597585398e954a4bea
Patrick Toohey <ptoohey@oswego.edu>
2013-06-27 16:18:43 -0400

    Roslyn demo project

---
+++ Roslyn/CompileDemo/Compiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompileDemo
+{
+    using System.IO;
+
+    public class Compiler
+    {
+        private string ExtractSource(string path)
+        {
+            string source;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                source = sr.ReadToEnd();
+            }
+
+            return source;
+        }
+    }
+}
