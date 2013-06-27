94bae74a7d9c7513afac19cb79fce29f4ee7094a
Patrick Toohey <ptoohey@oswego.edu>
2013-06-27 17:22:31 -0400

    self-hosted webapi sample

---
+++ WebApi/SelfHosted/Model/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace HttpClient.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Title { get; set; }
+    }
+}
