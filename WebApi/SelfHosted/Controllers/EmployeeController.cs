08071fe89476d3e15b11c7764fa94649a916c9ca
Patrick Toohey <ptoohey@oswego.edu>
2013-06-27 17:23:49 -0400

    self-hosted server implementation

--- WebApi/SelfHosted/Controllers/EmployeeController.cs
+++ WebApi/SelfHosted/Controllers/EmployeeController.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SelfHosted.Controllers
+﻿namespace SelfHosted.Controllers
 {
-    class EmployeeController
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using HttpClient.Model;
+
+    public class EmployeeController : ApiController
     {
+        static List<Employee> employees = InitEmployees();
+
+        public IEnumerable<Employee>  Get()
+        {
+            return employees;
+        }
+
+        public Employee Get(int id)
+        {
+            var employee = (from e in employees where e.Id == id select e).FirstOrDefault();
+
+            if (employee == null)
+            {
+                
+            }
+
+            return employee;
+        }
+
+        public void Post(Employee newHire)
+        {
+            employees.Add(newHire);
+        }
+
+        public HttpResponseMessage Put(int id, Employee employeeUpdate)
+        {
+            if (employeeUpdate == null)
+            {
+                return this.Request.CreateErrorResponse(HttpStatusCode.NotAcceptable, "");
+            }
+
+            var employee = (from e in employees where e.Id == id select e).FirstOrDefault();
+
+            if (employee == null)
+            {
+                return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, string.Format("Employee {0} not found.", id));
+            }
+
+            Console.WriteLine(string.Format("Employee: {0}", employeeUpdate.Id));
+            return this.Request.CreateResponse(HttpStatusCode.OK, employeeUpdate);
+        }
+
+        private static List<Employee> InitEmployees()
+        {
+            var ret = new List<Employee>();
+            ret.Add(new Employee { Id = 1, Name = "Don", Title = "Marketing Director" });
+            return ret;
+        }
     }
 }
--- WebApi/SelfHosted/Program.cs
+++ WebApi/SelfHosted/Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SelfHosted
+﻿namespace SelfHosted
 {
-    class Program
+    using System;
+    using System.Web;
+    using System.Web.Http;
+    using System.Web.Http.SelfHost;
+
+    internal class Program : HttpApplication
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var config = new HttpSelfHostConfiguration("http://127.0.0.1:8001");
+
+            config.Routes.MapHttpRoute(
+                "DefaultControllers", "Services/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            using (HttpSelfHostServer server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
         }
     }
 }
