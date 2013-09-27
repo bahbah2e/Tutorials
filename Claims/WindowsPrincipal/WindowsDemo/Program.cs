e302efe8fdcb8e84c994a04525d44a36ac2f122c
Patrick Toohey <ptoohey@oswego.edu>
2013-09-27 17:00:46 -0400

    Windows Identity demo

---
+++ Claims/WindowsPrincipal/WindowsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace WindowsDemo
+{
+    using System;
+    using System.Security.Principal;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = WindowsIdentity.GetCurrent();
+            Console.WriteLine(id.Name);
+
+            var account = new NTAccount(id.Name);
+            var sid = account.Translate(typeof(SecurityIdentifier));
+            Console.WriteLine(sid.Value);
+
+            foreach (var group in id.Groups.Translate(typeof(NTAccount)))
+            {
+                Console.WriteLine(group.Value);
+            }
+
+            var principle = new WindowsPrincipal(id);
+
+            var localAdmins = new SecurityIdentifier(
+                WellKnownSidType.BuiltinAdministratorsSid, 
+                id.User.AccountDomainSid);
+        }
+    }
+}
